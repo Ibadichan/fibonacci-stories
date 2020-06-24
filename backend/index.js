d2c238b2ae9911071e464d487f15b13194ba79ff
Ivan Badichan <badican01117@gmail.com>
2020-06-24 12:04:35 +0200

    Created one app-server

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,13 @@ const routes = require('./config/routes/index');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(express.static(path.resolve(__dirname, '../frontend/public')));
+app.use(
+  '/assets',
+  express.static(path.resolve(__dirname, '../frontend/public/assets'))
+);
 
+app.use(cors());
 app.use(requestIp.mw());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,6 +26,12 @@ app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 routes(app);
+
+app.get('*', (request, response) => {
+  response.sendFile(
+    path.resolve(__dirname, '../frontend/public/assets/index.html')
+  );
+});
 
 app.listen(port, () => {
   console.log(`Backend Server is listening on ${port}`); // eslint-disable-line
